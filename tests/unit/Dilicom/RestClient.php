bbb0ecebdb25591b1eac1a7cf13de75a42a6cec1
oservieres <olivier@servieres.info>
2014-04-10 11:38:15 +0200

    Add: availability check

--- tests/unit/Dilicom/RestClient.php
+++ tests/unit/Dilicom/RestClient.php
@@ -6,6 +6,7 @@ use atoum;
 
 class RestClient extends atoum\test
 {
+
     public function testConstructWithABadEnv()
     {
         $this
@@ -17,6 +18,91 @@ class RestClient extends atoum\test
                 ->isInstanceOf('\InvalidArgumentException')
                 ->message
                     ->contains("no server for env unknown")
+        ;
+    }
+
+    public function testOnBadlyFormattedEbooksGetEbooksAvailabilitiesThrowsException()
+    {
+        $response = "Availability response";
+        $this->mockGenerator->orphanize("__construct");
+        $connector = new \mock\Dilicom\HttpConnector();
+        $this->calling($connector)->get = $response;
+
+        $c = new \Dilicom\RestClient("user", "password", \Dilicom\RestClient::ENV_TEST);
+        $c->setConnector($connector);
+
+        $this
+            ->exception(
+                function() use ($c) {
+                    $c->getEbooksAvailabilities(array(
+                        array("9770000000000", "3230000000000", 5),
+                    ));
+                }
+            )
+            ->isInstanceOf('\InvalidArgumentException')
+            ->message
+            ->contains("Given ebook is badly formed")
+        ;
+    }
+
+    public function testWhenCalledProperlyGetEbooksAvailabilitiesReturnsExpectedResult()
+    {
+        $response = "Availability response";
+        $this->mockGenerator->orphanize("__construct");
+        $connector = new \mock\Dilicom\HttpConnector();
+        $this->calling($connector)->get = $response;
+
+        $c = new \Dilicom\RestClient("user", "password", \Dilicom\RestClient::ENV_TEST);
+        $c->setConnector($connector);
+
+        $availability = $c->getEbooksAvailabilities(array(
+            array("ean13" => "9780000000000", "glnDistributor" => "3330000000000", "unitPrice" => 7),
+            array("ean13" => "9770000000000", "glnDistributor" => "3230000000000", "unitPrice" => 5),
+        ));
+
+        $this->mock($connector)
+             ->call("get")
+             ->withArguments(
+                "/v1/hub-numerique-api/json/checkAvailability",
+                null,
+                array(
+                    "auth" => array("user", "password"),
+                    "verify" => true,
+                    "debug" => false,
+                    "query" => array(
+                        "checkAvailabilityLines[0].ean13" => "9780000000000",
+                        "checkAvailabilityLines[0].glnDistributor" => "3330000000000",
+                        "checkAvailabilityLines[0].unitPrice" => "7",
+                        "checkAvailabilityLines[1].ean13" => "9770000000000",
+                        "checkAvailabilityLines[1].glnDistributor" => "3230000000000",
+                        "checkAvailabilityLines[1].unitPrice" => "5",
+                    )
+                )
+             )
+             ->once()
+             ;
+
+        $this
+            ->string($availability)
+                ->isEqualTo($response)
+        ;
+    }
+
+    public function testGetEbookAvailability()
+    {
+        $response = "Availability response";
+        $this->mockGenerator->orphanize("__construct");
+        $connector = new \mock\Dilicom\HttpConnector();
+        $this->calling($connector)->get = $response;
+
+        $c = new \Dilicom\RestClient("user", "password", \Dilicom\RestClient::ENV_TEST);
+        $c->setConnector($connector);
+
+        $availability = $c->getEbookAvailability("9780000000000", "3330000000000", 749);
+
+        $this
+            ->string($availability)
+                ->isEqualTo($response)
         ;
     }
 
